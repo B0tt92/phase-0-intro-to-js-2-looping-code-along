4b342f383d420907c66811cc23ef5c2c85d7738f
+branden <+branden_ott@outlook.com+>
2021-10-03 14:13:33 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+ function writeCards( names, event ) {
+   let thank_you = []
+   for ( let i = 0; i < names.length; i++ ) {
+      thank_you.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!`) 
+      
+ }
+ return thank_you
+ 
+}
+
+
+function countDown(number) {
+  while (number >= 0) {
+      console.log(number);
+      number = number-1;
+    }
+}
